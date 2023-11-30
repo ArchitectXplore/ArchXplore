540f779217f5ac611c3fd9fbb0aba9a49fbe1991
Lutong Zhang <laytonzhang@foxmail.com>
2023-11-30 20:03:28 +0800

    Update sparta submodule

--- cmake/pybind11.cmake
+++ cmake/pybind11.cmake
@@ -3,10 +3,10 @@
 set(PYBIND11_DIR "${ArchXplore_BASE}/ext/pybind11")
 
 # Set up python
-find_package(Python3 COMPONENTS Interpreter Development)
+find_package(Python3 REQUIRED COMPONENTS Interpreter Development)
 
 add_subdirectory(${PYBIND11_DIR})
 
-include_directories (SYSTEM ${Python_INCLUDE_DIRS})
-
 list(APPEND ArchXplore_LIBS pybind11::headers Python3::Python)
+
+list(APPEND ArchXplore_INCLUDES ${Python3_INCLUDE_DIRS})
